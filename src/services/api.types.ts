fb10754296ec4eadc3359591882e6b01e0e15a57
Alex Santos <alexsantosquispe@gmail.com>
2025-05-20 15:45:54 -0400

    Fix: Remove albums page

--- src/services/api.types.ts
+++ src/services/api.types.ts
@@ -37,20 +37,6 @@ export interface PostComment {
   body: string;
 }
 
-export interface Album {
-  id: number;
-  userId: number;
-  title: string;
-}
-
-export interface Photo {
-  id: number;
-  albumId: number;
-  title: string;
-  url: string;
-  thumbnailUrl: string;
-}
-
 export interface Todo {
   id: number;
   userId: number;
