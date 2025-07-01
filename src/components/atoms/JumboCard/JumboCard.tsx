fbd150c00f778928c90288449a4ae5df69fc1a24
Alex Santos <alexsantosquispe@gmail.com>
2025-06-30 22:31:39 -0400

    screenshot updated

--- src/components/atoms/JumboCard/JumboCard.tsx
+++ src/components/atoms/JumboCard/JumboCard.tsx
@@ -56,7 +56,7 @@ export const JumboCard = ({ title, badgeLabel, endpoints }: JumboCardProps) => {
             initial={{ opacity: 0, height: 0 }}
             animate={{ opacity: 1, height: 'auto' }}
             exit={{ opacity: 0, height: 0 }}
-            transition={{ duration: 0.2 }}
+            transition={{ duration: 0.15 }}
             className="flex flex-col gap-y-2"
           >
             <span className="text-sm font-medium">Integrated endpoints</span>
